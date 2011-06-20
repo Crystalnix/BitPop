14b836626418080451f21b1d28540f25a9b22db7
Viatcheslav Gachkaylo <vgachkaylo@crystalnix.com>
2011-06-20 19:04:35 +0700

    Sparkle added to third_party. Made a .gyp file for Sparkle.

---
+++ third_party/Sparkle/postbuild_2.rb
@@ -0,0 +1,2 @@
+resources = "#{ENV["BUILT_PRODUCTS_DIR"]}/#{ENV["WRAPPER_NAME"]}/Resources"
+`ln -sfh "fr.lproj" "#{resources}/fr_CA.lproj"`
---
+++ third_party/Sparkle/postbuild_3.rb
@@ -0,0 +1,2 @@
+resources = "#{ENV["BUILT_PRODUCTS_DIR"]}/#{ENV["WRAPPER_NAME"]}/Resources"
+`ln -sfh "pt_BR.lproj" "#{resources}/pt.lproj"`
