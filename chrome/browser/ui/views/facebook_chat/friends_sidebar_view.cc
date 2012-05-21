41342908b1b33066bad80f9a5744e9eb12a0f1c0
Viatcheslav Gachkaylo <vgachkaylo@crystalnix.com>
2012-05-21 17:32:15 +0700

    Fixed notifications extension version. Set usage of proprietary codecs.

--- chrome/browser/ui/views/facebook_chat/friends_sidebar_view.cc
+++ chrome/browser/ui/views/facebook_chat/friends_sidebar_view.cc
@@ -9,22 +9,12 @@
 // Sidebar width
 static const int kFriendsSidebarWidth = 185;
 
-//scoped_ptr<TabContents> FriendsSidebarView::extension_page_contents_;
-
 FriendsSidebarView::FriendsSidebarView(Browser* browser, BrowserView *parent) :
   browser_(browser),
   parent_(parent) {
     set_id(VIEW_ID_FACEBOOK_FRIENDS_SIDE_BAR_CONTAINER);
     parent->AddChildView(this);
-    //if (!FriendsSidebarView::extension_page_contents_.get()) {
-    //  FriendsSidebarView::extension_page_contents_.reset(
-    //      new TabContents(browser->GetProfile(), NULL, MSG_ROUTING_NONE,
-    //        NULL, NULL));
-    //  FriendsSidebarView::extension_page_contents_->set_delegate(this);
-    //  FriendsSidebarView::extension_page_contents_->controller()
-    //     .LoadURL(GURL(kFriendsSidebarExtensionPageUrl),
-    //       GURL(), PageTransition::START_PAGE);
-    //}
+
     Init();
 }
 
