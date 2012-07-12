f98eb84b537fbacd9ab58204e48258907259f44e
Viatcheslav Gachkaylo <vgachkaylo@crystalnix.com>
2012-07-12 17:44:14 +0700

    Added wrench menu icons.

--- chrome/browser/ui/toolbar/wrench_menu_model.cc
+++ chrome/browser/ui/toolbar/wrench_menu_model.cc
@@ -43,6 +43,7 @@
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
+#include "grit/theme_resources_standard.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/button_menu_item_model.h"
@@ -510,8 +511,8 @@ void WrenchMenuModel::Build() {
   AddItem(IDC_VIEW_INCOMPATIBILITIES, l10n_util::GetStringUTF16(
       IDS_VIEW_INCOMPATIBILITIES));
 
-#if defined(OS_WIN)
   ResourceBundle& rb = ResourceBundle::GetSharedInstance();
+#if defined(OS_WIN)
   SetIcon(GetIndexOfCommandId(IDC_VIEW_INCOMPATIBILITIES),
           *rb.GetBitmapNamed(IDR_CONFLICT_MENU));
 #endif
@@ -524,6 +525,13 @@ void WrenchMenuModel::Build() {
     AddSeparator();
     AddItemWithStringId(IDC_EXIT, IDS_EXIT);
   }
+
+  SetIcon(GetIndexOfCommandId(IDC_BOOKMARKS_MENU), 
+          *rb.GetBitmapNamed(IDR_BOOKMARKS_FAVICON));
+  SetIcon(GetIndexOfCommandId(IDC_SHOW_DOWNLOADS), 
+          *rb.GetBitmapNamed(IDR_DOWNLOADS_FAVICON));
+  SetIcon(GetIndexOfCommandId(IDC_OPTIONS), 
+          *rb.GetBitmapNamed(IDR_SETTINGS_FAVICON));
 }
 #endif // !OS_CHROMEOS
 
